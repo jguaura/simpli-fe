38639de072572a15004ef4f34d1c85945890997a
Jose Rafael Guaura <jguaura@gmail.com>
2023-11-20 12:20:15 -0500

    refactor:
    - added error messages to form
    
    refactor:
    
    - UI tweaks

--- src/app/components/ProductDetailWrapper/InfoModal/InfoModalForm/InfoModalForm.tsx
+++ src/app/components/ProductDetailWrapper/InfoModal/InfoModalForm/InfoModalForm.tsx
@@ -22,32 +22,38 @@ function InfoModalContent() {
     const response = await createLead(data);
     setLeadResponse(response);
   };
-
+  const nameInputStyles = `${errors?.name?.message ? 'error' : ''} ${lato.className}`
+  const emailInputStyles = `${errors?.email?.message ? 'error' : ''} ${lato.className}`
   const submitBtnStyles = `${styles.submitBtn} btn`;
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
         <div className='form-control'>
           <label htmlFor='name'>Your name</label>
-          <input className={lato.className} {...register('name')} placeholder='Name' />
+          <input
+            className={nameInputStyles}
+            {...register('name', { required: 'Please enter your name' })}
+            placeholder='Name'
+          />
+          {errors.name && <span role='alert'>{errors.name.message}</span>}
         </div>
         <div className='form-control'>
           <label htmlFor='email'>Your email</label>
           <input
-            className={lato.className}
+            className={emailInputStyles}
             placeholder='Email'
             {...register('email', {
-              required: true,
+              required: 'Please enter your email',
               pattern: {
                 value: /\S+@\S+\.\S+/,
-                message: 'Entered value does not match email format'
+                message: 'Please enter a valid email adresss'
               }
             })}
           />
           {errors.email && <span role='alert'>{errors.email.message}</span>}
         </div>
         <button type='submit' className={submitBtnStyles}>
-          submit
+          Submit
         </button>
       </form>
     </div>
