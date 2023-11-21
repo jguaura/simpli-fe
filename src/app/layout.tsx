7c9d26a1b31c465c6c40e3495cc7b26b488012d4
Jose Rafael Guaura <jguaura@gmail.com>
2023-11-21 10:05:24 -0500

    refactor:
    - changed meta description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,9 @@ import Navbar from './components/Shared/Navbar/Navbar';
 const lato = Lato({ weight: ['400', '700'], subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Bikes world',
-  description: 'Lorem ipsum'
+  title: 'Vroom Vault',
+  description:
+    'Discover a world of speed and style at Vroom Vault – your premier destination for top-notch bikes and accessories. Explore our curated collection of high-performance bicycles and gear, meticulously selected to elevate your cycling experience. From sleek road bikes to rugged mountain bikes, find the perfect ride for every adventure. Browse through a treasure trove of accessories, from cutting-edge helmets to precision-engineered components. Unleash your passion for cycling with Vroom Vault – where quality meets velocity. Ride in style, ride with Vroom Vault'
 };
 
 export default function RootLayout({
